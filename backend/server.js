3ac2b69c688add4379dbd1f0856c4c07e1c67803
kennzuki <kingsyken@gmail.com>
2024-09-12 01:01:04 +0300

    connecting to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express();
 
-app.get('/',(res, req) => {
+app.get('/products',(res, req) => {
     req.send('sent the file')
 })
+
+console.log(process.env.MONGO_URI);
+
+
 app.listen(5000, ()=> {
     console.log('running on port');
     
