fe38410b250204a95ae6dda6cc771282ba70d7fe
kennzuki <kingsyken@gmail.com>
2024-09-12 19:54:54 +0300

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
-import express from "express";
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/products',(res, req) => {
-    req.send('sent the file')
-})
+
+app.get('/products', (res, req) => {
+  
+});
 
 console.log(process.env.MONGO_URI);
 
 
-app.listen(5000, ()=> {
-    console.log('running on port');
-    
-})
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
