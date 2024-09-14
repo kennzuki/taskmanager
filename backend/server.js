5f2454d3f03e275250c9323f20b4b98ec6771e69
kennzuki <kingsyken@gmail.com>
2024-09-15 02:23:54 +0300

    testing contoller and routes with database

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,41 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-import { connectDB } from "./config/db.js";
+import Product from './models/product.model.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
 
-app.get('/products', (res, req) => {
-  
+app.post('api/products', async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide all fields' });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error('Error in Create product:', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
 });
 
 console.log(process.env.MONGO_URI);
 
-
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+  console.log('Server started at http://localhost:' + PORT);
 });
