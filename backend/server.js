a8eee6cd78f5ff87cb3e7b50786f8dc544e76bbe
kennzuki <kingsyken@gmail.com>
2024-09-18 23:59:13 +0300

    frontend react install

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@ import dotenv from 'dotenv';
 
 import { connectDB } from './config/db.js';
 
-import productRoutes from "./routes/product.routes.js"
+import productRoutes from './routes/product.routes.js';
 
-const app = express();
 dotenv.config();
+const app = express();
+
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
-app.use("/api/products",productRoutes)
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
