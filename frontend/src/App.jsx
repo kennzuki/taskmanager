e4e3c9ff415ca4938f9a0a98bca1f5247263e0b2
kennzuki <kingsyken@gmail.com>
2024-10-03 19:43:03 +0300

    navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-
+import { Route, Routes } from 'react-router-dom';
+import CreatePage from './pages/CreatePage';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>
+        Hello world!
+      </h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-
+import { Link } from "react-router-dom";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import { useState } from "react";
 
 const Navbar = () => {
+  
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div>Navbar</div>
+    <div className="bg={useColorMode(bg-gray-100,bg-gray-900)} p-8 flex items-center justify-between gap-4 bg-gradient-to-r from-cyan-400 to-blue-500">
+      <h1 className="font-bold text-3xl"><Link to={"/"}>Product Store 🛒</Link></h1>
+      <ul className="flex gap-4 place-items-center text-white">
+      <Link className="" to='/create}'>
+       <button><FaRegPlusSquare size={40} /></button>
+      
+      </Link>
+      <button
+          onClick={toggleTheme}
+          className="px-4 py-2 bg-blue-500 text-white text-2xl rounded-md hover:bg-blue-600 dark:bg-yellow-500 dark:hover:bg-yellow-600 transition-colors duration-300"
+        >
+          {isDarkMode ? <LuSun/> :<IoMoon/> }
+        </button>
+      </ul>
+     
+    </div>
   )
 }
 
