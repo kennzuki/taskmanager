f53d7be29dad41b40863ee2a92800b5784213c0a
kennzuki <kingsyken@gmail.com>
2024-10-03 16:37:02 +0300

    swapped chakra with tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,11 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
 
-import CreatePage from "./pages/CreatePage";
-import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
 
 function App() {
-	return (
-		<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/create' element={<CreatePage />} />
-			</Routes>
-		</Box>
-	);
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
